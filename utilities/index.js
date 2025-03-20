5840f651d75d4cd047f1368ba794281d711ab5cb
sil <narradoresdigitales@gmail.com>
2025-03-20 04:48:16 -0500

    nav is working/displaying correctly

--- utilities/index.js
+++ utilities/index.js
@@ -38,12 +38,11 @@ Util.buildClassificationGrid = async function(data) {
     grid = '<ul id="inv-display">';
     data.forEach(vehicle => {
       console.log("Vehicle Data:", vehicle);  // Log full vehicle object
-      console.log("Image Path:", vehicle.inv_thumbnail);  // Log image path
-      
+      console.log("Image Path:", vehicle.inv_thumbnail);  // Log image path      
       grid += '<li>';
       grid += '<a href="../../inv/detail/' + vehicle.inv_id 
         + '" title="View ' + vehicle.inv_make + ' ' + vehicle.inv_model 
-        + ' details"><img src="#' + vehicle.inv_thumbnail 
+        + ' details"><img src="' + vehicle.inv_thumbnail 
         + '" alt="Image of ' + vehicle.inv_make + ' ' + vehicle.inv_model 
         + ' on CSE Motors" /></a>';
       grid += '<div class="namePrice">';
