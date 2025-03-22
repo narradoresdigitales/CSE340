caf5794a1495278ea2cf4c9ffaef57894d529200
sil <narradoresdigitales@gmail.com>
2025-03-22 09:27:30 -0500

    updated css for views

--- utilities/index.js
+++ utilities/index.js
@@ -36,11 +36,13 @@ Util.buildClassificationGrid = async function(data) {
       console.log("Vehicle Data:", vehicle);  // Log full vehicle object
       console.log("Image Path:", vehicle.inv_thumbnail);  // Log image path      
       grid += '<li>';
+      
       grid += '<a href="../../inv/detail/' + vehicle.inv_id 
         + '" title="View ' + vehicle.inv_make + ' ' + vehicle.inv_model 
         + ' details"><img src="' + vehicle.inv_thumbnail 
         + '" alt="Image of ' + vehicle.inv_make + ' ' + vehicle.inv_model 
         + ' on CSE Motors" /></a>';
+      
       grid += '<div class="namePrice">';
       grid += '<hr />';
       grid += '<h2>';
